b289383d9aaf0f0f71530982162fcec10922b309
Halton Bagley <haltonba94@gmail.com>
2020-01-03 21:35:14 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,25 +1,41 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+  outer_counter = 0
+  array_1 = []
+  while outer_counter < src.length do
+    inner_counter = 0
+    while inner_counter < src[outer_counter].length - 1 do
+      array_1.push "I love #{src[outer_counter][0]} and #{src[outer_counter][1]} on my pizza"
+      inner_counter += 1
+    end
+    outer_counter += 1
+  end
+  array_1
 end
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+  outer_counter = 0
+  array_2 = []
+  while outer_counter < src.length do
+    inner_counter = 0
+    while inner_counter < src[outer_counter].length - 1 do
+      src[outer_counter][0] > src[outer_counter][1] ? array_2 << src[outer_counter][0] : array_2 << src[outer_counter][1]
+      inner_counter += 1
+    end
+    outer_counter += 1
+  end
+  array_2
 end
 
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+  outer_counter = 0
+  total = 0
+  while outer_counter < src.length do
+    inner_counter = 0
+    while inner_counter < src[outer_counter].length - 1 do
+      src[outer_counter][0] % 2 === 0 && src[outer_counter][1] % 2 === 0 ? total += (src[outer_counter][0] + src[outer_counter][1]) : total += 0
+      inner_counter += 1
+    end
+    outer_counter += 1
+  end
+  total
 end
